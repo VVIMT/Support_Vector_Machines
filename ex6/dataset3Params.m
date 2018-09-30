366ac259ac6d664a3fd1907d45effad019b0e578
Vincent <vvimt@MacBook-Pro-de-Vincent.local>
2018-10-01 00:07:37 +0200

    ex6

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -23,11 +23,22 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+vec = [0.01 0.03 0.1 0.3 1 3 10 30]';
+err = -1;
 
+count = 1;
 
-
-
-
+for i = 1:length(vec)
+	for j = 1:length(vec)
+		model = svmTrain(X, y, vec(i), @(x1, x2) gaussianKernel(x1, x2, vec(j)));
+		predictions = svmPredict(model, Xval);
+		if mean(double(predictions ~= yval)) < err || err == -1
+			err = mean(double(predictions ~= yval));
+			C = vec(i);
+			sigma = vec(j);
+		endif;
+	end;
+end;
 
 % =========================================================================
 
